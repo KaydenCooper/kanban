eae6ae4e4bd884c81b9f94e6db94025da5399308
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-08-04 11:58:41 -0600

    small styling changes

--- clientsrc/src/views/Boards.vue
+++ clientsrc/src/views/Boards.vue
@@ -11,12 +11,19 @@
         </div>
       </form>
     </div>
-    <div v-for="board in boards" :key="board.id">
-      <router-link :to="{name: 'board', params: {boardId: board.id}}">
-        <h1>
-          <u>{{board.title}}</u>
-        </h1>
-      </router-link>
+    <div class="row p-2">
+      <div v-for="board in boards" :key="board.id">
+        <router-link :to="{name: 'board', params: {boardId: board.id}}">
+          <h1>
+            <div class="card text-white bg-dark shadow-lg mb-3 mx-2 p-2" style="max-width: 20rem;">
+              <div class="card-header">{{board.title}}</div>
+              <div class="card-body">
+                <p class="card-text">{{board.description}}</p>
+              </div>
+            </div>
+          </h1>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
