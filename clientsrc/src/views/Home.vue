c4fc678dac29ab3166a789e2c5a16ea683539494
Kayden Cooper <kayden_cooper@yahoo.com>
2020-08-05 17:03:57 -0600

    styling home page

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="home">
-    <h1>This is your home page.</h1>
+  <div class="home container-fluid flex-column flex-grow-1 justify-content-center bg-image">
+    <div class="row justify-content-center">
+      <div class="col-6 card bg-dark shadow-lg m-2 p-2">
+        <h1 class="text-light">Welcome to Harry & Kayden's KanBan Boards</h1>
+        <hr />
+        <h4 class="text-light">PLEASE LOG IN TO CREATE YOUR VERY OWN TRELLO BOARD</h4>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,10 +19,18 @@ export default {
   },
   computed: {},
   methods: {},
-  components: {}
+  components: {},
 };
 </script>
 
 
 <style scoped>
+.bg-image {
+  background-image: url("https://format-com-cld-res.cloudinary.com/image/private/s--7deqf97A--/c_limit,g_center,h_65535,w_2500/fl_keep_iptc.progressive,q_95/v1/90f977a493f4f27c80949284ee718157/bw_crested_butte_mts_no_cropping.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100vw;
+  margin-left: -15px;
+}
 </style>
